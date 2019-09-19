b5129705d816c6c1d7ede1a3c960f81544c9eadb
David Jacobs <djacobs74@gmail.com>
2019-09-18 17:53:15 -0700

    restructuring

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class App extends React.Component {
+
+	render() {
+		return (
+			<div className="">
+				Welcome to the Rativerse!
+				
+		          	
+			           
+		          
+
+	
+			</div>
+	
+		);
+	}
+}
+
+export default App;
