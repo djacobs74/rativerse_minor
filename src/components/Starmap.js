5cdfd19000ecca07aeadcefa3b4c2f5d511274ba
David Jacobs <djacobs74@gmail.com>
2019-10-09 12:38:34 -0700

    add utils and map key fix

--- src/components/Starmap.js
+++ src/components/Starmap.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createMap } from '../actions/map';
+// import { getSector } from './_utils/movement';
 
 class StarMap extends Component {
 
@@ -18,13 +19,19 @@ class StarMap extends Component {
 		return ''
 	}
 
+	// getSector = (x, y) => {
+	//   console.log('getsector');
+	// };
+
+
+
 	render () {
 		const mapData = this.props.map;
 		return (
 			<div>
 				
 				{mapData.map((m, index) => (
-					<div className={`sectorWrapper ${this.oddEven(m['x'])}`} sector={`x: ${m['x']} y: ${m['y']}`}>
+					<div className={`sectorWrapper ${this.oddEven(m['x'])}`} sector={`x: ${m['x']} y: ${m['y']}`} key={index}>
 						<div className={`sector sectorTop`}></div>
 		    			<div className={`sector sectorMiddle`} key={index}>{`${m['x']}, ${m['y']}`}</div>
 		    			<div className={`sector sectorBottom`}></div>
---
+++ src/components/_utils/movement.js
@@ -0,0 +1,3 @@
+export const getSector = (x, y) => {
+  console.log('getsector');
+}
