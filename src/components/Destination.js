214ef68ca9cfa69714180a65743237402bde1e08
David Jacobs <djacobs74@gmail.com>
2019-10-25 11:41:29 -0700

    Destination refactor

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -9,19 +9,26 @@ class Destination extends React.Component {
 		super(props);
 		this.state = {value: '', destination: '', position: [0, 0]};
 
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
+		// this.handleChange = this.handleChange.bind(this);
+		// this.handleSubmit = this.handleSubmit.bind(this);
   	}
 
-  	handleChange(event) {
-		this.setState({value: event.target.value});
-		console.log('HANDLECHANGE', this.state.destination);
-  	}
+  // 	handleChange(event) {
+		// this.setState({value: event.target.value});
+		// console.log('HANDLECHANGE', this.state.destination);
+		
+  // 	}
 
-  	handleSubmit(event) {
+  // 	handleSubmit(event) {
+  // 		this.setState({destination: this.props.sector});
+		// console.log('SET DESTINATION', this.props.sector);
+		// event.preventDefault();
+  // 	}
+
+  	setDestination(event) {
   		this.setState({destination: this.props.sector});
-		console.log('SET DESTINATION', this.props.sector);
-		event.preventDefault();
+  		getPath(this.state.position, this.props.sector);
+  		event.preventDefault();
   	}
 
   	// MARTEL DRIVE STARTS MOVEMENT FUNCTION HERE. IF POSITION != DESTINATION, CALL MOVE FUNCTION AGAIN.
@@ -31,15 +38,15 @@ class Destination extends React.Component {
   	render() {
 		return (
 			<div>
-		  		<form onSubmit={this.handleSubmit}>
-					<label>
-			  			Set Destination To Selected Sector: 
-			  			<input className="moveLabelInput" type="text" value={this.props.sector} onChange={this.handleChange} />
-					</label>
-					<input className="moveLabelInput" type="submit" value="Set Destination" />
-					<div>Destination: {prettyCoords(this.state.destination)}</div>
-		  		</form>
-	  			<button onClick={(position, destination) => getPath(this.state.position, this.state.destination)}>Engage Martel Drive</button>
+				<div>
+		  			Set Destination To Selected Sector: 
+		  			{/*<input className="moveLabelInput" type="text" value={this.props.sector} onChange={this.handleChange} />*/}
+		  			<button className="moveLabelInput" onClick={(event) => this.setDestination(event)}>Set Destination</button>
+				</div>
+				{/*<input className="moveLabelInput" type="submit" value="Set Destination" onChange={this.handleChange}/>*/}
+				<div>Destination: {prettyCoords(this.state.destination)}</div>
+		  		
+	  			{/*<button onClick={(position, destination) => getPath(this.state.position, this.state.destination)}>Engage Martel Drive</button>*/}
 	  		</div>
 		);
   	}
