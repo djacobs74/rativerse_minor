cb3aeeffb6710b24919f56bcb297ee998d4d66d4
David Jacobs <djacobs74@gmail.com>
2019-12-11 20:13:23 -0800

    pathing update

--- src/actions/moveShip.js
+++ src/actions/moveShip.js
@@ -1,10 +1,18 @@
 import { moveCheck } from '.././components/_utils/movement';
+import { getPath } from '../actions/getPath';
 
 export const moveShip = (position, path) => {
 
 	const pathLength = path.length - 1;
 	const destination = path[pathLength];
 	let counter = 0;
+	const nullPosition = null;
+	const nullDestination = null;
+
+
+	function getNewPath (newPath) {
+		getPath(nullPosition, nullDestination, newPath);
+	}
 	// debugger;
 
 	console.log('PATH', path);
@@ -12,11 +20,19 @@ export const moveShip = (position, path) => {
 	return (dispatch) => {
 		function moveDelay () {
 			setTimeout(function () {
-				position = path[counter];
+				position = path[0];
 				dispatch({type: 'MOVE_SHIP', payload: position});
 				console.log('SHIP MOVED, NEW POSITION = ', position);
-				if (counter < pathLength) {
-					counter++;
+				if (path.length > 1) {
+					if(position[0] === path[0][0] && position[1] === path[0][1]) {
+						let removed = path.splice(0, 1);
+					
+						getNewPath(path);
+						// debugger;
+					}
+
+
+					// counter++;
 
 					moveDelay();
 					
