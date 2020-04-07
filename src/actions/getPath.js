956134d3bd0252fcd206101729c01a464ae14c05
David Jacobs <djacobs74@gmail.com>
2020-04-06 17:22:46 -0700

    shortest path

--- src/actions/getPath.js
+++ src/actions/getPath.js
@@ -5,84 +5,106 @@ export const getPath = (position, destination, newPath) => {
 	// PUT TIMEOUT HERE? WILL BE BASED ON MARTEL DRIVE, FOR NOW DEFAULT TIME
 
 	// Math.sign(3)
-	let path = [];
-	
+	let path;
+	let counter = 0;
+	let pathOptions = [];
 	if(position && destination) {
-		let posX = position[0];
-		let posY = position[1];
-		const destX = destination[0];
-		const destY = destination[1];
-		while (posX !== destX || posY !== destY) {
-			// console.log('posX, posY', posX, posY);
-			// console.log('starting while loop');
-			let rangeOneResults = rangeOne(posX, posY);
-			// console.log('Range One', rangeOneResults);
-			let moveOptions = [];
-			let newCoords = [];
+		while (counter < 50) {
+		
+			let posX = position[0];
+			let posY = position[1];
+			const destX = destination[0];
+			const destY = destination[1];
 
-			if (posX < destX) {
-				if ( ((destX - posX) >= 3 ) || ((destY - posY) >= 1 ) ) {
-					console.log('moving down-right');
-					moveOptions.push(rangeOneResults.bottomRight);
+			let pathOption = {};
+			path = [];
+			
+			while (posX !== destX || posY !== destY) {
+				// console.log('posX, posY', posX, posY);
+				// console.log('starting while loop');
+				let rangeOneResults = rangeOne(posX, posY);
+				// console.log('Range One', rangeOneResults);
+				let moveOptions = [];
+				let newCoords = [];
+
+				if (posX < destX) {
+					if ( ((destX - posX) >= 3 ) || ((destY - posY) >= 1 ) ) {
+						console.log('moving down-right');
+						moveOptions.push(rangeOneResults.bottomRight);
+					}
+				} 
+				if (posX < destX) {
+					if ( ((destX - posX) >= 3 ) || ((destY - posY) <= 0 ) ) {
+						console.log('moving down-left');
+						moveOptions.push(rangeOneResults.bottomLeft);
+					}
 				}
-			} 
-			if (posX < destX) {
-				if ( ((destX - posX) >= 3 ) || ((destY - posY) <= 0 ) ) {
-					console.log('moving down-left');
-					moveOptions.push(rangeOneResults.bottomLeft);
+				if (destX < posX) {
+					if (((destY <= posY)) || (((posX - destX) >= 3) && ((destY - posY ) <= 1))) {
+						console.log('moving top-left');
+						moveOptions.push(rangeOneResults.topLeft);
+					}	
 				}
-			}
-			if (destX < posX) {
-				if (((destY <= posY)) || (((posX - destX) >= 3) && ((destY - posY ) <= 1))) {
-					console.log('moving top-left');
-					moveOptions.push(rangeOneResults.topLeft);
-				}	
-			}
-			if (destX < posX) {
-				if (((destY >= posY)) || (((posX - destX) >= 3) && ((destY - posY ) >= 1))) {
-					console.log('moving top-right');
-					moveOptions.push(rangeOneResults.topRight);
+				if (destX < posX) {
+					if (((destY >= posY)) || (((posX - destX) >= 3) && ((destY - posY ) >= 1))) {
+						console.log('moving top-right');
+						moveOptions.push(rangeOneResults.topRight);
+					}
 				}
-			}
-			if (destY < posY) {
-				if ((posX === destX) || ((destY - posY) <= 3)) {
-					console.log('moving left');
-					moveOptions.push(rangeOneResults.left);
+				if (destY < posY) {
+					if ((posX === destX) || ((destY - posY) <= 3)) {
+						console.log('moving left');
+						moveOptions.push(rangeOneResults.left);
+					}
 				}
-			}
-			if (destY > posY) {
-				if ((posX === destX) || ((destY - posY) >= 3)) {
-					console.log('moving right');
-					moveOptions.push(rangeOneResults.right);
+				if (destY > posY) {
+					if ((posX === destX) || ((destY - posY) >= 3)) {
+						console.log('moving right');
+						moveOptions.push(rangeOneResults.right);
+					}
 				}
+
+				let option = 0;
+
+				if (moveOptions.length > 1) {
+					option = Math.floor(Math.random() * Math.floor(2));
+				}
+
+				newCoords = moveOptions[option];
+				path.push(newCoords);
+
+				posX = newCoords[0];
+				posY = newCoords[1];
+
+				console.log('move options', moveOptions);
+				console.log('moving to newCoords', newCoords);
 			}
-
-			let option = 0;
-
-			if (moveOptions.length > 1) {
-				option = Math.floor(Math.random() * Math.floor(2));
-			}
-
-			newCoords = moveOptions[option];
-			path.push(newCoords);
-
-			posX = newCoords[0];
-			posY = newCoords[1];
-
-			console.log('move options', moveOptions);
-			console.log('moving to newCoords', newCoords);
+			pathOption = path;
+			pathOptions.push(pathOption);
+			counter ++;
+		
 		}
 	}
+
+	let shortestPath = pathOptions[0];
+ 	pathOptions.map(p => {
+ 		if (p.length < shortestPath.length) {
+ 			shortestPath = p;
+ 		}
+ 	})
 	
+ 	path = shortestPath;
 
 	if(!position && !destination) {
 		if(newPath) {
 			path = newPath;
 		}
 	}
+
+	
 	console.log('PATH', path);
 
-  	// debugger;
+  	debugger;
   	// return {type: 'PATH_SET', payload: path};
   	return (dispatch) => {
   		dispatch({type: 'PATH_SET', payload: path});
