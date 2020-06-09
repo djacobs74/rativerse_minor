a7cb70d96b0ed43c9ff98a4b6eb68663f0983901
David Jacobs <djacobs74@gmail.com>
2020-06-08 18:16:14 -0700

    added start btn

--- src/components/Setup.js
+++ src/components/Setup.js
@@ -17,18 +17,21 @@ class Setup extends Component {
 	state = {
 		startGame: false,
 		selectedShip: '',
-		selectedFaction: 'Select a Faction'
+		selectedFaction: 'Select a Faction',
+		gameReady: false
 	}
 
 	selectedFaction = (option) =>  {
-		
-		this.setState({startGame: true, selectedFaction: option})
+		this.setState({selectedFaction: option, gameReady: true})
+	}
+
+	startGame() {
+		this.setState({startGame: true})
 	}
 
 
 	render () {
 		const options = FACTIONS;
-		const defaultFactionOption = 'Select a Faction';
 		let startGame = this.state.startGame;
 	
 		console.log('START GAME STATE', startGame);
@@ -42,7 +45,8 @@ class Setup extends Component {
 					<div className="homePage">
 						<div className="welcome">Welcome to the Rativerse!</div>
 						
-						<Dropdown options={options} onChange={this.selectedFaction} value={defaultFactionOption} placeholder="Select an option" />
+						<Dropdown options={options} onChange={this.selectedFaction} value={this.state.selectedFaction} placeholder="Select an option" />
+						<button disabled={!this.state.gameReady} className="startBtn" onClick={() => this.startGame()}>Start Game</button>
 						<FactionDescriptions />
 					</div>
 
