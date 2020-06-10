fe975b77c308560e7c7745ba8e1923e75fd249cd
David Jacobs <djacobs74@gmail.com>
2020-06-10 11:14:47 -0700

    npc move ship randomization

--- src/components/_utils/npcShipMovement.js
+++ src/components/_utils/npcShipMovement.js
@@ -5,51 +5,55 @@ import { pathCheck } from './movement';
 export const npcShipMover = (npcShips, playerFaction) => {
 	// return npcShips with updated locations
 	let npcShipsActive = [];
+	const moveOptionNum = 5;
 
 	npcShips.map(s => {
-		let posX = s.x;
-		let posY = s.y;
-		let moveOptions = [];
-		let newCoords = [];
-		let rangeOneResults = rangeOne(posX, posY);
+		const moveChance = Math.floor(Math.random() * Math.floor(moveOptionNum));
+		if(moveChance == (moveOptionNum-1)) {
+			let posX = s.x;
+			let posY = s.y;
+			let moveOptions = [];
+			let newCoords = [];
+			let rangeOneResults = rangeOne(posX, posY);
 
-		if(pathCheck(rangeOneResults.bottomRight)) {
-			moveOptions.push(rangeOneResults.bottomRight);
+			if(pathCheck(rangeOneResults.bottomRight)) {
+				moveOptions.push(rangeOneResults.bottomRight);
+			}
+			
+			if(pathCheck(rangeOneResults.bottomLeft)) {
+				moveOptions.push(rangeOneResults.bottomLeft);
+			}
+
+			if(pathCheck(rangeOneResults.topLeft)) {
+				moveOptions.push(rangeOneResults.topLeft);
+			}
+
+			if(pathCheck(rangeOneResults.topRight)) {
+				moveOptions.push(rangeOneResults.topRight);
+			}
+
+			if(pathCheck(rangeOneResults.left)) {
+				moveOptions.push(rangeOneResults.left);
+			}
+
+			if(pathCheck(rangeOneResults.right)) {
+				moveOptions.push(rangeOneResults.right);
+			}
+
+			let option = 0;
+			
+			let length = moveOptions.length;
+			if (moveOptions.length) {
+				option = Math.floor(Math.random() * Math.floor(length));
+			}
+			// debugger;
+			newCoords = moveOptions[option];
+			// path.push(newCoords);
+
+			s.x = newCoords[0];
+			s.y = newCoords[1];
 		}
-		
-		if(pathCheck(rangeOneResults.bottomLeft)) {
-			moveOptions.push(rangeOneResults.bottomLeft);
-		}
-
-		if(pathCheck(rangeOneResults.topLeft)) {
-			moveOptions.push(rangeOneResults.topLeft);
-		}
-
-		if(pathCheck(rangeOneResults.topRight)) {
-			moveOptions.push(rangeOneResults.topRight);
-		}
-
-		if(pathCheck(rangeOneResults.left)) {
-			moveOptions.push(rangeOneResults.left);
-		}
-
-		if(pathCheck(rangeOneResults.right)) {
-			moveOptions.push(rangeOneResults.right);
-		}
-
-		let option = 0;
-		
-		let length = moveOptions.length;
-		if (moveOptions.length) {
-			option = Math.floor(Math.random() * Math.floor(length));
-		}
-		// debugger;
-		newCoords = moveOptions[option];
-		// path.push(newCoords);
-
-		s.x = newCoords[0];
-		s.y = newCoords[1];
-
+			
 		npcShipsActive.push(s);
 
 		// set hostile here?
