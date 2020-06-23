8529048311bed9b30250fd2c297106b70c09a4ee
David Jacobs <djacobs74@gmail.com>
2020-06-22 17:59:05 -0700

    wip cargo buy / sell

--- src/components/DockedControlPanel.js
+++ src/components/DockedControlPanel.js
@@ -12,6 +12,14 @@ import { connect } from 'react-redux';
 
 class DockedControlPanel extends Component {
 
+	state = {
+		cargoOptions: []
+	}
+
+	// componentDidMount = () => {
+	// 	this.setState({cargoOptions: })
+	// }
+
 	getDockingArea(sector) {
 		let dockingArea = [];
 		this.props.map.map(m => {
@@ -23,14 +31,44 @@ class DockedControlPanel extends Component {
 		return dockingArea
 	}
 
+	updateCargo(tradeGood, amount) {
+		let cargo = this.state.cargoOptions.find((c) => c.value === tradeGood.value);
+		const cargoDetails = {value: tradeGood.value, amount: amount, price: tradeGood.buyPrice || tradeGood.sellPrice }
+		if (!cargo) {
+			this.state.cargoOptions.push(cargoDetails);
+		} else {
+			cargo.amount += amount;
+
+			// debugger;
+			this.setState({cargoOptions:  [cargo]});
+		}
+		// PROBLEM HERE
+		console.log('CARGO OPTIONS', this.state.cargoOptions);
+	}
+
+	transAction(t, cargoOptions) {
+		let shipCargo = this.props.currentShip.cargoHold;
+		let shipCargoCount = this.props.currentShip.cargo;
+		const transActionType = t.buyPrice ? 'buy' : 'sell';
+		debugger;
+
+	}
+
+	getTotal(tradeGood, cargoOptions) {
+		let cargoTotal = cargoOptions.find((c) => c.value === tradeGood.value);
+		console.log('CARGO TOTAL', cargoTotal);
+		cargoTotal = cargoTotal ? cargoTotal.amount : null;
+		return cargoTotal
+	}
+
 
 	render () {
-		
+		// console.log('CARGO OPTIONS', this.state.cargoOptions);
 		console.log('CURRENT SHIP', this.props.currentShip);
 		
 		
 		const currentShip = this.props.currentShip;
-
+		const cargoOptions = this.state.cargoOptions;
 		const dockingArea = this.getDockingArea(this.props.currentPosition);
 		const tradeGoods = dockingArea ? dockingArea[0].tradeGoods : null;
 
@@ -49,11 +87,28 @@ class DockedControlPanel extends Component {
 
 				{tradeGoods.length
     				? tradeGoods.map(t =>
+
     					<div key={dockingArea[0].id + t.value} className='tradeGoodWrapper'>
 	    					<div>{t.label}</div>
 	    					<div>{t.buyPrice && `Buying at ${t.buyPrice}  (min: ${t.minPrice}  max: ${t.maxPrice})`}</div>
 	    					<div>{t.sellPrice && `Selling at ${t.sellPrice}  (min: ${t.minPrice}  max: ${t.maxPrice})`}</div>
 	    					<div>{`Amount ${t.amount} (max amount: ${t.maxAmount})`}</div>
+	    					{t.buyPrice ? 
+	    						<div>Add to Cart
+	    							<button onClick={() => this.updateCargo(t, 1)}>1</button>
+	    							<button onClick={() => this.updateCargo(t, 5)}>5</button>
+	    							<button onClick={() => this.updateCargo(t, 10)}>10</button>
+	    							<button onClick={() => this.updateCargo(t, t.amount)}>All</button>
+	    							<button onClick={() => {this.transAction(t, this.state.cargoOptions)}}>Buy</button>{this.getTotal(t, cargoOptions)}
+	    						</div> : 
+	    						<div>Add to Cart
+	    							<button onClick={() => this.updateCargo(t, 1)}>1</button>
+	    							<button onClick={() => this.updateCargo(t, 5)}>5</button>
+	    							<button onClick={() => this.updateCargo(t, 10)}>10</button>
+	    							<button onClick={() => this.updateCargo(t, t.amount)}>All</button>
+	    							<button onClick={() => {this.transAction(t, this.state.cargoOptions)}}>Sell</button>{this.getTotal(t, cargoOptions)}
+	    						</div>
+	    					} 
 	    				</div>
     				) : <div></div>
 				}
