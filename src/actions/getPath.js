921c4ac55727cb2a103dd9913bda1609b138655f
David Jacobs <djacobs74@gmail.com>
2020-06-24 10:18:36 -0700

    path fix

--- src/actions/getPath.js
+++ src/actions/getPath.js
@@ -108,12 +108,16 @@ export const getPath = (position, destination, newPath) => {
 		}
 	}
 
-	let shortestPath = pathOptions[0];
- 	pathOptions.map(p => {
- 		if (p.length < shortestPath.length) {
- 			shortestPath = p;
- 		}
- 	})
+	// let shortestPath = pathOptions[0];
+	// console.log('pathOptions', pathOptions);
+	// console.log('shortestPath', shortestPath);
+	// console.log('REDUCE PATHS', pathOptions.reduce((prev, next) => prev.length > next.length ? next : prev));
+	let shortestPath = pathOptions.reduce((prev, next) => prev.length > next.length ? next : prev);
+ 	// pathOptions.map(p => {
+ 	// 	if (p.length < shortestPath.length) {
+ 	// 		shortestPath = p;
+ 	// 	}
+ 	// })
 	
  	path = shortestPath;
 
