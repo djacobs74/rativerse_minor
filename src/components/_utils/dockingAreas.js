1cdedd018f1a6810dbc8882cfb68bb66a78de2d6
David Jacobs <djacobs74@gmail.com>
2020-06-25 11:55:25 -0700

    buy sell fix

--- src/components/_utils/dockingAreas.js
+++ src/components/_utils/dockingAreas.js
@@ -7,49 +7,53 @@ export const createDockingAreas = (map) => {
 	const typesArray = ['Planet', 'Space Station', 'Asteroid Base'];
 	const spaceTypeArray = [{name: 'Solar System', value: 'solarSystem'}, {name: 'Open Space', value: 'openSpace'}, {name: 'Asteroid Belt', value: 'asteroidBelt'}];
 
+	
+
 	function addTradeGoods () {
-		const tradeGoodsTypesCount = TRADE_GOODS.length;
+		const tgCopy = _.cloneDeep(TRADE_GOODS);
+		const tradeGoodsTypesCount = tgCopy.length;
 		let tradeGoods = [];
-		const guaranteedGoodIndex = Math.floor(Math.random() * Math.floor(tradeGoodsTypesCount));
+		// const guaranteedGoodIndex = Math.floor(Math.random() * Math.floor(tradeGoodsTypesCount));
 
-		tradeGoods.push(TRADE_GOODS[guaranteedGoodIndex]);
+		// tradeGoods.push(tgCopy[guaranteedGoodIndex]);
 
-		TRADE_GOODS.map(t => {
-			const randomGoodIndex = Math.floor(Math.random() * Math.floor(tradeGoodsTypesCount));
-			if(randomGoodIndex > guaranteedGoodIndex ) {
-				if(t !== TRADE_GOODS[guaranteedGoodIndex]) {
-					tradeGoods.push(t);
-				}
-			}
-		})
+		let currentIndex = tgCopy.length, temporaryValue, randomIndex;
+		 while (0 !== currentIndex) {
+		    randomIndex = Math.floor(Math.random() * currentIndex);
+		    currentIndex -= 1;
 
-		tradeGoods.map(p => {
-			// THIS WORKS, BUT FOR SOME REASON ITS LOOPING THROUGH THIS MORE THAN ONCE . . .
-			if ( !(('buyPrice' in p) || ('sellPrice' in p)) ) {
-				// debugger;
-				const torF = _.sample([true, false]);
-				console.log('torF ', typeof torF );
-				const min = Math.ceil(p.minPrice);
-			  	const max = Math.floor(p.maxPrice);
-			  	const price = Math.floor(Math.random() * (max - min + 1)) + min;
-			  	const amount = Math.floor(Math.random() * (200 - 0 + 1)) + 0;
-			  	p.amount = amount;
-			  	if(torF === true) {
-			  		// debugger;
-			  		p.sellPrice = price;
-			  	} else if (torF !== true) {
-			  		// debugger;
-			  		p.buyPrice = price;
-			  	}
-			}
+		    temporaryValue = tgCopy[currentIndex];
+		    tgCopy[currentIndex] = tgCopy[randomIndex];
+		    tgCopy[randomIndex] = temporaryValue;
+	    
+		 }
 
-		})
-		// console.log('TRADE GOODS', tradeGoods);
+
+		 let randomGoodIndex = Math.floor(Math.random() * Math.floor(tradeGoodsTypesCount));
+		 while (0 !== randomGoodIndex) {
+		 	let tradeGoodItem = tgCopy[randomGoodIndex];
+		 	let buyOrSell = _.sample([true, false]);
+		 	const min = Math.ceil(tradeGoodItem.minPrice);
+		  	const max = Math.floor(tradeGoodItem.maxPrice);
+		  	const price = Math.floor(Math.random() * (max - min + 1)) + min;
+		  	const amount = Math.floor(Math.random() * (200 - 0 + 1)) + 0;
+		  	tradeGoodItem.amount = amount;
+		 	if(buyOrSell === true) {
+		 	
+		  		tradeGoodItem.sellPrice = price;
+		  	} else if (buyOrSell !== true) {
+		  	
+		  		tradeGoodItem.buyPrice = price;
+		  	}
+		 	randomGoodIndex -= 1;
+		 	tradeGoods.push(tradeGoodItem);
+		 }
+
 		return tradeGoods
 	}
 
-
 	map.map(m => {
+	
 		m.dockingArea = [];
 		m.sectorType = [];
 		m.sectorType.push(spaceTypeArray[1]);
@@ -64,28 +68,22 @@ export const createDockingAreas = (map) => {
 			m.sectorType[0] = spaceTypeArray[1]; // open space
 		}
 
-
-
-
 		if (randomNum >= 9) {
 			id++;
 			const typeNum = Math.floor(Math.random() * Math.floor(3));
 			// console.log('typeNum', typeNum);
 			
 			const type = typesArray[typeNum];
-
 			const tradeGoods = addTradeGoods();
 			let dockingArea = {id: id, type: type, tradeGoods: tradeGoods};
-
-
-			       
+       
 			m.sectorType[0] = spaceTypeArray[typeNum];
-
 			m.dockingArea.push(dockingArea);
 		}
 			
 
 	})
+
 	return map
 }
 
